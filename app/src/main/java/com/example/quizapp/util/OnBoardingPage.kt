f18f790cdc609b29968cb880e90a2b3d86108192
dancanyego <56153056+dancanyego@users.noreply.github.com>
2022-11-11 21:56:38 +0300

    ViewModel

--- app/src/main/java/com/example/quizapp/util/OnBoardingPage.kt
+++ app/src/main/java/com/example/quizapp/util/OnBoardingPage.kt
@@ -1,2 +1,29 @@
 package com.example.quizapp.util
 
+import androidx.annotation.DrawableRes
+import com.example.quizapp.R
+
+sealed class OnBoardingPage(
+    @DrawableRes
+    val image: Int,
+    val title: String,
+    val description: String
+) {
+    object First : OnBoardingPage(
+        image = R.drawable.first,
+        title = "The Best Quizes",
+        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod."
+    )
+
+    object Second : OnBoardingPage(
+        image = R.drawable.second,
+        title = "Coordination",
+        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod."
+    )
+
+    object Third : OnBoardingPage(
+        image = R.drawable.third,
+        title = "Dialogue",
+        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod."
+    )
+}
