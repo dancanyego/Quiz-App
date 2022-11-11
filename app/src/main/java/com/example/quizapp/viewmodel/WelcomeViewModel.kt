b21fec5c1aea8b4ef5c7077dc2964c7685e21974
dancanyego <56153056+dancanyego@users.noreply.github.com>
2022-11-11 22:02:27 +0300

    onboarding sceens

--- app/src/main/java/com/example/quizapp/viewmodel/WelcomeViewModel.kt
+++ app/src/main/java/com/example/quizapp/viewmodel/WelcomeViewModel.kt
@@ -1,2 +1,23 @@
 package com.example.quizapp.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WelcomeViewModel @Inject constructor(
+    private val repository: DataStoreRepository
+) : ViewModel() {
+
+    fun saveOnBoardingState(completed: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.saveOnBoardingState(completed = completed)
+        }
+    }
+
+}
+
