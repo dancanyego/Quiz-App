1b304f1a085ce804cc490cd7794a274c9cbdedb6
dancanyego <56153056+dancanyego@users.noreply.github.com>
2022-11-15 12:57:53 +0300

    login page

--- app/src/main/java/com/example/quizapp/screen/HomeScreen.kt
+++ app/src/main/java/com/example/quizapp/screen/HomeScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination
+import androidx.navigation.Navigator
 
 @Composable
 fun HomeScreen() {
--- app/src/main/java/com/example/quizapp/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/example/quizapp/viewmodel/SplashViewModel.kt
@@ -3,10 +3,11 @@ package com.example.quizapp.viewmodel
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.graphics.BlendMode.Companion.Screen
+
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.quizapp.data.DataStoreRepository
+import com.example.quizapp.navigation.Screen
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,22 +18,13 @@ class SplashViewModel @Inject constructor(
     private val _isLoading: MutableState<Boolean> = mutableStateOf(true)
     val isLoading: State<Boolean> = _isLoading
 
-    private val _startDestination: MutableState<String> = mutableStateOf(com.example.quizapp.navigation.Screen.Welcome.route)
+    private val _startDestination: MutableState<String> = mutableStateOf(Screen.Welcome.route)
     val startDestination: State<String> = _startDestination
 
     init {
         viewModelScope.launch {
-            repository.readOnBoardingState().collect { completed ->
-                if (completed) {
-                    _startDestination.value = com.example.quizapp.navigation.Screen.Home.route
-                } else {
-                    _startDestination.value = com.example.quizapp.navigation.Screen.Welcome.route
-                }
-            }
-            _isLoading.value = false
+
         }
     }
 
 }
-
-
